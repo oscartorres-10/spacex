5ce19a7c6ef3b3a1ee1700eb4dec7518af54b03f
oscartorres <38195378+oscartorres-10@users.noreply.github.com>
2024-07-16 20:03:14 -0300

    fix(PastLaunches, LaunchCard): fix issues on dark mode, remove bg-white on cards and add text-black on input

--- src/components/LaunchCard.tsx
+++ src/components/LaunchCard.tsx
@@ -8,7 +8,7 @@ const LaunchCard = ({ name, details, date_local, rocket }: Launch) => {
   const relativeDate = formatRelative(isoDate, new Date())
   return (
     <>
-      <article className='grid gap-4 rounded-sm m-4 p-2 border w-1/3 bg-white border-gray-500 font-mono'>
+      <article className='grid gap-4 rounded-sm m-4 p-2 border w-1/3 border-gray-500 font-mono'>
         <div>
           <p className='font-bold text-2xl'>{name} </p>
           <p className='font-extralight'>
--- src/components/PastLaunches.tsx
+++ src/components/PastLaunches.tsx
@@ -16,7 +16,7 @@ const PastLaunches = (data: any) => {
       <h1 className='text-4xl font-bold my-4'>List of Past Launches</h1>
       <input
         placeholder='Search...'
-        className='flex h-10 w-1/3 rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50'
+        className='flex h-10 w-1/3 rounded-md border border-input text-black px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50'
         onChange={(event) => setSearchTerm(event.target.value)}
       />
 
