ca449b889c730e05275a3571a6fa7ff256b57a2c
oscartorres <38195378+oscartorres-10@users.noreply.github.com>
2024-07-19 16:43:09 -0300

    refactor(queries): move body to a const and then pass it with JSON.stringify(body)

--- src/queries.tsx
+++ src/queries.tsx
@@ -1,4 +1,13 @@
 export async function getPastLaunches() {
+  const body = {
+    query: {},
+    options: {
+      limit: 3,
+      sort: {
+        date_local: 'asc',
+      },
+    },
+  }
   // the sorting doesn't seem to be working, i'm not sure if the API blocked me and it's serving always the same data but it's not reflecting when I change either the sort or the limit
   // FIXED the issue was i wasn't sending the headers in the request, and Next.js cached an initial request i did and was showing always the same with 10 total launches, this confused me a bit, now the limit and sort reflect correctly when i change them
   const res = await fetch('https://api.spacexdata.com/v4/launches/query', {
@@ -6,15 +15,7 @@ export async function getPastLaunches() {
     headers: {
       'Content-Type': 'application/json',
     },
-    body: `{ 
-      "query": {}, 
-      "options": {
-        "limit": 5,
-        "sort": {
-          "date_local": "desc"
-        }
-      } 
-    }`,
+    body: JSON.stringify(body),
     cache: 'no-store',
   })
   if (!res.ok) {
