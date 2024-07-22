092ca57284253c5feb5c09354e4aa0df089eac6c
oscartorres <38195378+oscartorres-10@users.noreply.github.com>
2024-07-22 18:01:03 -0300

    refactor(layout): update favicon and title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,31 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Space X',
+  title: 'Space X by oscartorres.io',
   description: '',
 }
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>{children}</body>
+      <head>
+        {/* an emoji as favicon
+         * @see https://favicons.joshuasoileau.com/
+         */}
+        <link
+          rel='icon'
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='100'>🚀</text></svg>"
+        />
+      </head>
     </html>
-  );
+  )
 }
